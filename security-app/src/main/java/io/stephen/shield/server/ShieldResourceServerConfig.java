3906ce660fd94927b025eff498491e331403a1ac
Stephenzho <stephen_zho@126.com>
2018-08-31 16:46:58 +0800

    增加资源服务器

---
+++ security-app/src/main/java/io/stephen/shield/server/ShieldResourceServerConfig.java
@@ -0,0 +1,67 @@
+package io.stephen.shield.server;
+
+import io.stephen.shield.app.authentication.openid.OpenIdAuthenticationSecurityConfig;
+import io.stephen.shield.core.authentication.FormAuthenticationConfig;
+import io.stephen.shield.core.authentication.mobile.SmsCodeAuthenticationSecurityConfig;
+import io.stephen.shield.core.authorize.AuthorizeConfigManager;
+import io.stephen.shield.core.validate.code.ValidateCodeSecurityConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.social.security.SpringSocialConfigurer;
+
+/**
+ * @author zhoushuyi
+ * @date 2018/8/31
+ */
+@Configuration
+@EnableResourceServer
+public class ShieldResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Autowired
+    protected AuthenticationSuccessHandler imoocAuthenticationSuccessHandler;
+
+    @Autowired
+    protected AuthenticationFailureHandler imoocAuthenticationFailureHandler;
+
+    @Autowired
+    private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
+
+    @Autowired
+    private OpenIdAuthenticationSecurityConfig openIdAuthenticationSecurityConfig;
+
+    @Autowired
+    private ValidateCodeSecurityConfig validateCodeSecurityConfig;
+
+    @Autowired
+    private SpringSocialConfigurer imoocSocialSecurityConfig;
+
+    @Autowired
+    private AuthorizeConfigManager authorizeConfigManager;
+
+    @Autowired
+    private FormAuthenticationConfig formAuthenticationConfig;
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+
+        formAuthenticationConfig.configure(http);
+
+        http.apply(validateCodeSecurityConfig)
+                .and()
+                .apply(smsCodeAuthenticationSecurityConfig)
+                .and()
+                .apply(imoocSocialSecurityConfig)
+                .and()
+                .apply(openIdAuthenticationSecurityConfig)
+                .and()
+                .csrf().disable();
+
+        authorizeConfigManager.config(http.authorizeRequests());
+    }
+
+}
